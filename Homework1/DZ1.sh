0372d6713507aeb265a354cf7aab0789c28d4526
Kate <kate@localhost>
2021-12-05 01:00:58 +0300

    homework1

---
+++ Homework1/DZ1.sh
@@ -0,0 +1,20 @@
+# 3
+cd a_test
+
+#4
+mkdir in_dir in_dirr in_dirrr
+
+#5
+cd in_dir
+
+#6
+touch text_1.txt text_2.txt text_3.txt j_1.json j_2.json
+
+#7
+mkdir p_1 p_2 p_3
+
+#8
+ls
+
+#13
+mv text_1.txt j_1.json p_1
