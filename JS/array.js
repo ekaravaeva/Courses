541e4f57f4007dffb218edd06cf4dfda97710de4
Kate <kate@localhost>
2022-01-10 15:45:05 +0300

    add devtools

---
+++ JS/array.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,5,'ab'];
+console.log(arr);
+
+for (let element of arr) {
+	console.log(element*10);
+}
---
+++ JS/object.js
@@ -0,0 +1,19 @@
+let o = {
+	name: 'Kate4',
+	age: 35,
+	salary: {
+		amount: 450,
+		currency: 'USD'
+	},
+	children: ['d', 'k']
+};
+
+//console.log(o);
+
+//console.log(o.name);
+
+//console.log(o.salary);
+
+console.log(o.salary.amount);
+
+console.log(o.children[0]);
